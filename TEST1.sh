5ed251870b02a06787b69f883be7fdb430818c1d
Akshay Jagdale <114833898+akshayjagdale1010@users.noreply.github.com>
2026-01-09 06:31:03 +0530

    Add files via upload

---
+++ TEST1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "thois is the errror script"
+echo "this is my system path $path "
+
+
+#set a jdk home  envirement variable
+export JDK_HOME=bin/jdk
+echo "my new jdk home is =$JDK_HOME"
